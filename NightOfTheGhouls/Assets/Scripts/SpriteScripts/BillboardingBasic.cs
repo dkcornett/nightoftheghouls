d0fa91a7d2721b1cbf3936a3c1ab488ba271a62c
LlynPrinton <Llynprinton@gmail.com>
2023-07-05 21:29:09 -0400

    Wrangling sprites in a 3d space

---
+++ NightOfTheGhouls/Assets/Scripts/SpriteScripts/BillboardingBasic.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardingBasic : MonoBehaviour
+{
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    }
+}
